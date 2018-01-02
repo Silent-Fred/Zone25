a2355124d571fbda6f4d0b1853cd0a3b1d7c558d
Michael Kühweg <developer@kuehweg.de>
2018-01-02 20:24:46 +0100

    Initial Commit

---
+++ Zone25Tests/Zone25Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Zone25Tests.swift
+//  Zone25Tests
+//
+//  Created by Michael Kühweg on 02.01.18.
+//  Copyright © 2018 Michael Kühweg. All rights reserved.
+//
+
+import XCTest
+@testable import Zone25
+
+class Zone25Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
